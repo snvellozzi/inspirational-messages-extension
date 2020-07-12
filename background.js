e6bd768c76626ea80659a39ff2bc87179bac8d53
Sophia Vellozzi <snvello@gmail.com>
2020-07-12 19:47:46 -0400

    Add another color button

--- background.js
+++ background.js
@@ -1,17 +1,9 @@
-//tests button color & functionality
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#6FCBEC'}, function() {
-        console.log("hello");
-    });
-
-    
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
       });
-  });
--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
-//sets color of button on popup
+//sets color of button on popup  
 let colorChange = document.getElementById('colorChange');
-
+let colorChange2 = document.getElementById('colorChange2');
 
 chrome.storage.sync.get('color', function(data) {
   colorChange.style.backgroundColor = data.color;
   colorChange.setAttribute('value', data.color);
-
 });
 
 colorChange.onclick = function(element) {
@@ -16,4 +15,14 @@ colorChange.onclick = function(element) {
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+
+colorChange2.onclick = function(element){
+    let color2 = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color2 + '";'});
+  });
+
+};
 
